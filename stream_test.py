7b4c492d3fe564c1b263fb98160e2d2764c65860
abe-mart <19436449+abe-mart@users.noreply.github.com>
2022-04-26 20:51:25 -0600

    Add watermark

--- stream_test.py
+++ stream_test.py
@@ -10,6 +10,7 @@ from main import addShadow
 ### Excluding Imports ###
 st.title("Background Putter")
 
+use_water = st.checkbox('Put Watermark')
 uploaded_file = st.file_uploader("Choose an image...", type="png",accept_multiple_files=False)
 if uploaded_file is not None:
     img = Image.open(uploaded_file)
@@ -25,9 +26,12 @@ if uploaded_file is not None:
     # Background 1 - Board
     foreground = ImageOps.contain(img,(2580,1186))
     background = Image.open('Images/Background1.jpg', 'r')
+    watermark = Image.open('Images/Watermark_short.png','r')
     bg_w, bg_h = background.size
     
     background = addShadow(foreground,background,x_offset=0,y_offset=-50,x_blur_offset=0,y_blur_offset=0,lighten_amount=0,blur_amount=0,alpha_reduction=3.5)
+    
+    if use_water: background.paste(watermark, (0,0), watermark)
     
     font = ImageFont.truetype(font='Fonts/Bebas.ttf',size=248)
     draw = ImageDraw.Draw(im=background)
@@ -41,8 +45,11 @@ if uploaded_file is not None:
     # Background 2 - Boxes
     foreground = ImageOps.contain(img,(1800,1800))
     background = Image.open('Images/Background2.jpg', 'r')
+    watermark = Image.open('Images/Watermark.png','r')
     
     background = addShadow(foreground,background,x_offset=300,y_offset=0,x_blur_offset=3,y_blur_offset=-3)
+    
+    if use_water: background.paste(watermark, (0,0), watermark)
     
     img_byte_arr = io.BytesIO()
     background.save(img_byte_arr, format='jpeg')
@@ -52,8 +59,11 @@ if uploaded_file is not None:
     # Background 3 - Kitchen
     foreground = ImageOps.contain(img,(2195,915))
     background = Image.open('Images/Background3.jpg', 'r')
+    watermark = Image.open('Images/Watermark.png','r')
     
     background = addShadow(foreground,background,x_offset=-180,y_offset=-500,x_blur_offset=3,y_blur_offset=-3,lighten_amount=50,blur_amount=8,alpha_reduction=3)
+    
+    if use_water: background.paste(watermark, (0,0), watermark)
     
     img_byte_arr = io.BytesIO()
     background.save(img_byte_arr, format='jpeg')
@@ -64,10 +74,12 @@ if uploaded_file is not None:
     foreground = ImageOps.contain(img,(2597,1186))
     background = Image.open('Images/Background4a.jpg', 'r')
     front = Image.open('Images/Background4b.png', 'r')
+    watermark = Image.open('Images/Watermark.png','r')
     
     background = addShadow(foreground,background,x_offset=0,y_offset=-300,x_blur_offset=-3,y_blur_offset=1,lighten_amount=50,blur_amount=8,alpha_reduction=2.5)
     
     background.paste(front, (0,0), front)
+    if use_water: background.paste(watermark, (0,0), watermark)
     
     img_byte_arr = io.BytesIO()
     background.save(img_byte_arr, format='jpeg')
@@ -78,10 +90,12 @@ if uploaded_file is not None:
     foreground = ImageOps.contain(img,(2294,1005))
     background = Image.open('Images/Background5a.jpg', 'r')
     front = Image.open('Images/Background5b.png', 'r')
+    watermark = Image.open('Images/Watermark.png','r')
     
     background = addShadow(foreground,background,x_offset=-220,y_offset=-250,x_blur_offset=-1,y_blur_offset=0,lighten_amount=50,blur_amount=6,alpha_reduction=3.5)
     
     background.paste(front, (0,0), front)
+    if use_water: background.paste(watermark, (0,0), watermark)
     
     img_byte_arr = io.BytesIO()
     background.save(img_byte_arr, format='jpeg')
@@ -92,10 +106,12 @@ if uploaded_file is not None:
     foreground = ImageOps.contain(img,(968,671))
     background = Image.open('Images/Background6a.jpg', 'r')
     front = Image.open('Images/Background6b.png', 'r')
+    watermark = Image.open('Images/Watermark.png','r')
     
     background = addShadow(foreground,background,x_offset=50,y_offset=200,x_blur_offset=0,y_blur_offset=0,lighten_amount=40,blur_amount=1.5,alpha_reduction=50)
     
     background.paste(front, (0,0), front)
+    if use_water: background.paste(watermark, (0,0), watermark)
     
     img_byte_arr = io.BytesIO()
     background.save(img_byte_arr, format='jpeg')
@@ -106,10 +122,12 @@ if uploaded_file is not None:
     foreground = ImageOps.contain(img,(1005,481))
     background = Image.open('Images/Background7a.jpg', 'r')
     front = Image.open('Images/Background7b.png', 'r')
+    watermark = Image.open('Images/Watermark.png','r')
     
     background = addShadow(foreground,background,x_offset=25,y_offset=-200,x_blur_offset=0,y_blur_offset=0,lighten_amount=20,blur_amount=0,alpha_reduction=1)
     
     background.paste(front, (0,0), front)
+    if use_water: background.paste(watermark, (0,0), watermark)
     
     img_byte_arr = io.BytesIO()
     background.save(img_byte_arr, format='jpeg')
@@ -119,11 +137,11 @@ if uploaded_file is not None:
     # Background 8 - Car
     foreground = ImageOps.contain(img,(1392,408))
     background = Image.open('Images/Background8.jpg', 'r')
-    # front = Image.open('Images/Background7b.png', 'r')
+    watermark = Image.open('Images/Watermark.png','r')
     
     background = addShadow(foreground,background,x_offset=-210,y_offset=-525,x_blur_offset=1,y_blur_offset=1,lighten_amount=10,blur_amount=1,alpha_reduction=1)
     
-    # background.paste(front, (0,0), front)
+    if use_water: background.paste(watermark, (0,0), watermark)
     
     img_byte_arr = io.BytesIO()
     background.save(img_byte_arr, format='jpeg')
